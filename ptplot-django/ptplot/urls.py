7ac05b17e8cecddd3d7b636a77dd8130fe96f1ba
David Weir <david.weir@helsinki.fi>
2018-04-12 10:06:56 +0300

    Load images separately

--- ptplot-django/ptplot/urls.py
+++ ptplot-django/ptplot/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('snr', views.snr_image, name='snr'),
+    path('ps', views.ps_image, name='ps'),
     path('', views.ptplot_form, name='index'),
 ]
--- ptplot-django/ptplot/views.py
+++ ptplot-django/ptplot/views.py
@@ -14,7 +14,9 @@ from .powerspectrum import get_PS_image
 
 def inline_image(image_sio):
 
-    return base64.b64encode(image_sio.read()).decode()
+#    return base64.b64encode(image_sio.read()).decode()
+
+    return image_sio.read()
     
 #    imgStr = "data:image/svg+xml;base64,"
 
@@ -23,39 +25,62 @@ def inline_image(image_sio):
 #    return("""
 #    <img src="%s"></img>
 #    """ % (imgStr))
-    
 
+def ps_image(request):
+    if request.method == 'GET':
+        form = PTPlotForm(request.GET)
+
+        if form.is_valid():
+            vw = form.cleaned_data['vw']
+            tstar = form.cleaned_data['tstar']
+
+            sio_PS = get_PS_image(Tstar=tstar, vw=vw) # , usetex=usetex)
+            ps = inline_image(sio_PS)
+            return HttpResponse(ps, content_type="image/svg+xml")
+
+        
+def snr_image(request):
+    if request.method == 'GET':
+        form = PTPlotForm(request.GET)
+
+        if form.is_valid():
+            vw = form.cleaned_data['vw']
+            tstar = form.cleaned_data['tstar']
+
+            sio_SNR = get_SNR_image(Tstar=tstar, vw=vw) # , usetex=usetex)
+            snr = inline_image(sio_SNR)
+            return HttpResponse(snr, content_type="image/svg+xml")
+    
 def ptplot_form(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    if request.method == 'GET':
         # create a form instance and populate it with data from the request:
-        form = PTPlotForm(request.POST)
+        form = PTPlotForm(request.GET)
 
         # check whether it's valid:
         if form.is_valid():
             vw = form.cleaned_data['vw']
             tstar = form.cleaned_data['tstar']
-#            usetex = form.cleaned_data['usetex']
+            querystring = request.GET.urlencode()
+            #            usetex = form.cleaned_data['usetex']
 
-            start = time.time()
+#            start = time.time()
             
-            sio_PS = get_PS_image(Tstar=tstar, vw=vw) # , usetex=usetex)
-            ps = inline_image(sio_PS)
-            sio_SNR = get_SNR_image(Tstar=tstar, vw=vw) # , usetex=usetex)
-            snr = inline_image(sio_SNR)
 
-            took = "{0:0.1f}".format(time.time() - start)
+
+
+#            took = "{0:0.1f}".format(time.time() - start)
             
             template = loader.get_template('ptplot/ptplotresult.html')
-            context = {'form': form, 'ps': ps, 'snr': snr, 'took': took}
+            context = {'form': form,
+                       'querystring': querystring}
             return HttpResponse(template.render(context, request))
 
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        template = loader.get_template('ptplot/ptplot.html')
-        form = PTPlotForm()
-        context = {'form': form}
-        return HttpResponse(template.render(context, request))
+        
+    template = loader.get_template('ptplot/ptplot.html')
+    form = PTPlotForm()
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
 
     
