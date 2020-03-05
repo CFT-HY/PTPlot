f8aae9a51598781ad62b1bae1a20e99047d5ad98
David Weir <david.weir@helsinki.fi>
2020-03-05 12:16:57 -0500

    Updated setup.py to reflect version, 1.0.1, and url, ptplot.org

--- setup.py
+++ setup.py
@@ -9,16 +9,17 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ptplot',
-    version='0.1',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
+        'django'
     ],
     include_package_data=True,
     license='BSD License',  # example license
     description='PTPlot tool for plotting phase transitions',
     long_description=README,
-    url='http://www.helsinki.fi/~weir/',
+    url='http://www.ptplot.org/',
     author='David Weir',
     author_email='david.weir@helsinki.fi',
     classifiers=[
