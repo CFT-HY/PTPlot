1be644b7a9019099262d07c487850499f63c87c8
David Weir <david.weir@helsinki.fi>
2021-02-12 16:59:07 +0200

    Rewrote URLs which return SVGs or CSVs to have the relevant ending in the URL (.svg, .csv), to make things a bit nicer for the user.

--- ptplot/urls.py
+++ ptplot/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     # CSV
 
-    path(r'csv',
+    path(r'curvedata.csv',
          views.csv,
          name='csv'),
 
@@ -13,15 +13,15 @@ urlpatterns = [
     # Plots
 
     # SNR plot with alpha on the x-axis and beta on the y-axis
-    path(r'snr_alphabeta',
+    path(r'snr_alphabeta.svg',
          views.snr_alphabeta_image,
          name='snr_alphabeta'),
     # SNR plot with UbarF on the x-axis and H_n R_* on the y-axis
-    path(r'snr',
+    path(r'snr.svg',
          views.snr_image,
          name='snr'),
     # Power spectrum plot with sensitivity curve superimposed
-    path(r'ps',
+    path(r'ps.svg',
          views.ps_image,
          name='ps'),
 
@@ -36,6 +36,10 @@ urlpatterns = [
 
     # Plot many points - manual input
     path(r'multiple',
+         views.multiple,
+         name='multiple'),
+
+    path(r'multiple.svg',
          views.multiple,
          name='multiple'),
 
@@ -65,22 +69,22 @@ urlpatterns = [
          name='model_point_plot'),
 
     # Display an individual model point on the SNR and PS plots
-    path(r'models/<int:model_id>/<int:point_id>/snr',
+    path(r'models/<int:model_id>/<int:point_id>/snr.svg',
          views.model_point_snr,
          name='model_point_snr'),
 
     # Display an individual model point on the SNR and PS plots
-    path(r'models/<int:model_id>/<int:point_id>/snr_alphabeta',
+    path(r'models/<int:model_id>/<int:point_id>/snr_alphabeta.svg',
          views.model_point_snr_alphabeta,
          name='model_point_snr_alphabeta'),
 
     # Display an individual model point on the SNR and PS plots
-    path(r'models/<int:model_id>/<int:point_id>/ps',
+    path(r'models/<int:model_id>/<int:point_id>/ps.svg',
          views.model_point_ps,
          name='model_point_ps'),
 
     # Download CSV data for an individual model point
-    path(r'models/<int:model_id>/<int:point_id>/csv',
+    path(r'models/<int:model_id>/<int:point_id>/curvedata.csv',
          views.model_point_csv,
          name='model_point_csv'),
 
@@ -91,12 +95,12 @@ urlpatterns = [
          name='model_scenario_plot'),
 
     # Display a group of scenario points on the SNR plots
-    path(r'models/<int:model_id>/scenarios/<int:scenario_id>/snr',
+    path(r'models/<int:model_id>/scenarios/<int:scenario_id>/snr.svg',
          views.model_scenario_snr,
          name='model_scenario_snr'),
 
     # Display a group of scenario points on the SNR plots
-    path(r'models/<int:model_id>/scenarios/<int:scenario_id>/snr_alphabeta',
+    path(r'models/<int:model_id>/scenarios/<int:scenario_id>/snr_alphabeta.svg',
          views.model_scenario_snr_alphabeta,
          name='model_scenario_snr_alphabeta'),
 
@@ -105,11 +109,11 @@ urlpatterns = [
     
     
     
-    path(r'models/<int:model_id>/snr',
+    path(r'models/<int:model_id>/snr.svg',
          views.model_snr,
          name='model_snr'),
     
-    path(r'models/<int:model_id>/snr_alphabeta',
+    path(r'models/<int:model_id>/snr_alphabeta.svg',
          views.model_snr_alphabeta,
          name='model_snr_alphabeta'),
 
