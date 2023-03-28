59802e75570a9b74bad1d17a6321ad67a9b6968f
Deanna Hooper <deanna.hooper@helsinki.fi>
2023-03-28 12:53:54 +0300

    Start adding docstrings for espinosa.py

--- ptplot/science/espinosa.py
+++ ptplot/science/espinosa.py
@@ -1,11 +1,55 @@
+"""Energy budget computations.
+
+This file contains all the functions related to the calculation of
+the energy budget of a First-order Phase Transition, following José R.
+Espinosa et al. JCAP06 (2010) 028 (arXiv:1004.4187).
+
+Contains the following functions:
+    * ubarf - calculates ubarf from alpha and vw
+    * kappav - calculates kappav from alpha and vw
+    * ubarf_to_alpha - converts from ubarf to alpha
+"""
+
 import math
 import scipy.optimize
 import numpy as np
 
 def ubarf(vw, alpha):
+    """Calculates the rms fluid velocity.
+
+    Parameters
+    ----------
+    vw : float
+        Wall velocity
+    alpha : float
+        Phase transition strength
+
+    Returns
+    -------
+    ubarf : float
+        Measure of the rms fluid velocity
+    """
     return math.sqrt((3.0/4.0)*kappav(vw,alpha)*alpha/(1.0 + alpha))
 
 def kappav(vw, alpha):
+    """Calculates the fluid efficiency.
+
+    The fluid efficiency gives the fraction of vacuum energy that is
+    turned into kinetic energy during the phase transition. These
+    equations can be found in Appendix A of arXiv:1004.4187.
+
+    Parameters
+    ----------
+    vw : float
+        Wall velocity
+    alpha : float
+        Phase transition strength
+
+    Returns
+    -------
+    kappav : float
+        Fluid efficiency
+    """
 
     kappaA = math.pow(vw,6.0/5.0)*6.9*alpha/ \
              (1.36 - 0.037*math.sqrt(alpha) + alpha)
@@ -13,13 +57,13 @@ def kappav(vw, alpha):
              (0.017 + math.pow(0.997 + alpha,2.0/5.0))
     kappaC = math.sqrt(alpha)/(0.135 + math.sqrt(0.98 + alpha))
     kappaD = alpha/(0.73 + 0.083*math.sqrt(alpha) + alpha)
-    
+
     cs = math.pow(1.0/3.0,0.5)
 
     xiJ = (math.sqrt((2.0/3.0)*alpha + alpha*alpha) + math.sqrt(1.0/3.0))/(1+alpha)
 
     deltaK = -0.9*math.log((math.sqrt(alpha)/(1 + math.sqrt(alpha))))
-            
+
     if vw < cs:
         return math.pow(cs,11.0/5.0)*kappaA*kappaB/ \
                 ((math.pow(cs,11.0/5.0)
@@ -34,7 +78,7 @@ def kappav(vw, alpha):
         return kappaB + (vw - cs)*deltaK \
                 + (math.pow(vw-cs,3.0)/math.pow(xiJ-cs,3.0))*(kappaC-kappaB-(xiJ-cs)*deltaK)
 
-    
+
 def ubarf_to_alpha(vw, this_ubarf):
     def ubarf_to_alpha_inner(vw, this_ubarf):
 
@@ -47,6 +91,6 @@ def ubarf_to_alpha(vw, this_ubarf):
 #            import sys
 #            sys.stderr.write('vw=%g, this_ubarf=%g\n' % (vw, this_ubarf))
 
-    
+
     vfunc = np.vectorize(ubarf_to_alpha_inner)
     return vfunc(vw, this_ubarf)
