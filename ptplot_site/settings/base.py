8b85f0a15a563fe563bcac7d1f6909b15814ecb1
Mika Mäki <mika.x.maki@helsinki.fi>
2025-01-29 18:33:01 +0200

    Fix PTPlot reference in INSTALLED_APPS

--- ptplot_site/settings/base.py
+++ ptplot_site/settings/base.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ptplot',
+    'ptplot.apps.PTPlotConfig',
 ]
 
 MIDDLEWARE = [
