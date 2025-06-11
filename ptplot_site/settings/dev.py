d0128d00f772ee5fa37b0a41b044c39d92582336
Mika Mäki <mika.x.maki@helsinki.fi>
2025-06-11 17:18:08 +0300

    Replace single quotes with double quotes

--- ptplot_site/settings/dev.py
+++ ptplot_site/settings/dev.py
@@ -7,6 +7,6 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 INTERNAL_IPS = ["127.0.0.1"]
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5)qw0w+z9)*q145l8$n_@d+$dj0f++p)npdmrddazr5j$yu869'
+SECRET_KEY = "django-insecure-5)qw0w+z9)*q145l8$n_@d+$dj0f++p)npdmrddazr5j$yu869"
 
 TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
