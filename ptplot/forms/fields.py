9f052f0683bb7ca51ebbda9eeb8401f8d9b5ddef
Mika Mäki <mika.x.maki@helsinki.fi>
2025-06-11 15:25:15 +0300

    Create field classes for the different field types

--- ptplot/forms/fields.py
+++ ptplot/forms/fields.py
@@ -1,16 +1,56 @@
+import typing as tp
+
 from django import forms
+from django.db.models import QuerySet
+from django.core.exceptions import ValidationError
+
+from ptplot.models import Model
 from ptplot.science.mission_profile import MISSION_PROFILE_CHOICES
 
 
+def validate_velocity(value: float) -> None:
+    if not (0 < value <= 1):
+        raise ValidationError(f"{value} must be 0 < value <= 1")
+
+
+class AlphaField(forms.FloatField):
+    def __init__(
+            self,
+            label: str = r"Phase transition strength $\alpha_\theta$",
+            min_value: float = 0.,
+            **kwargs):
+        super().__init__(label=label, min_value=min_value, **kwargs)
+
+
+class BetaOverHField(forms.FloatField):
+    def __init__(
+            self,
+            label: str = r"Inverse phase transition duration $\beta/H_*$",
+            min_value: float = 0.,
+            **kwargs):
+        super().__init__(label=label, min_value=min_value, **kwargs)
+
+
+class GStarField(forms.FloatField):
+    def __init__(
+            self,
+            label: str = r"Degrees of freedom $g_\star$",
+            min_value: float = 0.,
+            **kwargs):
+        super().__init__(label=label, min_value=min_value, **kwargs)
+
+
 class MissionProfileField(forms.TypedChoiceField):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(
-            *args,
-            label=r"Mission profile",
+    def __init__(
+            self,
+            label: str = r"Mission profile",
             choices=MISSION_PROFILE_CHOICES,
             coerce=int,
             empty_value=None,
-            **kwargs
+            **kwargs):
+        super().__init__(
+            label=label, choices=choices, coerce=coerce,
+            empty_value=empty_value, **kwargs
         )
 
     # def to_python(self, value) -> tp.Optional[MissionProfile]:
@@ -18,3 +58,41 @@ class MissionProfileField(forms.TypedChoiceField):
     #         return None
     #     converted = int(super().to_python(value))
     #     return MissionProfile.from_ind(converted)
+
+
+class ModelField(forms.ModelChoiceField):
+    def __init__(
+            self,
+            queryset: QuerySet = None,
+            label: str = "Model",
+            **kwargs):
+        super().__init__(
+            queryset=Model.objects.all() if queryset is None else queryset,
+            label=label,
+            **kwargs
+        )
+
+
+class TStarField(forms.FloatField):
+    def __init__(
+            self,
+            label: str = r"Transition temperature $T_\star$",
+            min_value: float = 0.,
+            **kwargs):
+        super().__init__(label=label, min_value=min_value, **kwargs)
+
+
+class VWField(forms.FloatField):
+    def __init__(
+            self,
+            label: str = r"Wall velocity $v_\mathrm{w}$",
+            min_value: float = 0.,
+            max_value: float = 1.,
+            validators: tp.Sequence[tp.Callable] = (validate_velocity, ),
+            **kwargs):
+        super().__init__(
+            label=label,
+            min_value=min_value, max_value=max_value,
+            validators=validators,
+            **kwargs
+        )
--- ptplot/forms/forms.py
+++ ptplot/forms/forms.py
@@ -1,51 +1,18 @@
 import logging
 
-from django import forms
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-from ptplot.forms.fields import MissionProfileField
+from ptplot.forms.fields import *
 from ptplot.models import Model
 from ptplot.science.mission_profile import MissionProfile
 
 logger = logging.getLogger(__name__)
 
 
-def validate_velocity(value: float) -> None:
-    if not ((value > 0.0) and (value <= 1.0)):
-        raise ValidationError(
-            _("%(value)s must be greater than zero and less than or equal to 1"),
-            params={"value": value},
-        )
-
-
 class PTPlotForm(forms.Form):
-    vw = forms.FloatField(
-        label=r"Wall velocity $v_\mathrm{w}$",
-        min_value=0.0, max_value=1.0,
-        validators=[validate_velocity],
-        localize=False
-    )
-    alpha = forms.FloatField(
-        label=r"Phase transition strength $\alpha_\theta$",
-        min_value=0.0,
-        localize=False
-    )
-    beta_over_H = forms.FloatField(
-        label=r"Inverse phase transition duration $\beta/H_*$",
-        min_value=0.0,
-        localize=False
-    )
-    T_star = forms.FloatField(
-        label=r"Transition temperature $T_\star$",
-        min_value=0.0,
-        localize=False
-    )
-    g_star = forms.FloatField(
-        label=r"Degrees of freedom $g_\star$",
-        min_value=0.0,
-        localize=False
-    )
+    vw = VWField()
+    alpha = AlphaField()
+    beta_over_H = BetaOverHField()
+    T_star = TStarField()
+    g_star = GStarField()
     mission_profile_ind = MissionProfileField()
     # engine = forms.ChoiceField()
     # usetex = forms.BooleanField(
@@ -63,22 +30,9 @@ class PTPlotForm(forms.Form):
 
 
 class MultipleForm(forms.Form):
-    vw = forms.FloatField(
-        label=r"Wall velocity $v_\mathrm{w}$",
-        min_value=0.0, max_value=1.0,
-        validators=[validate_velocity],
-        localize=False
-    )
-    T_star = forms.FloatField(
-        label=r"Transition temperature $T_\star$",
-        min_value=0.0,
-        localize=False
-    )
-    g_star = forms.FloatField(
-        label=r"Degrees of freedom $g_\star$",
-        min_value=0.0,
-        localize=False
-    )
+    vw = VWField()
+    T_star = TStarField()
+    g_star = GStarField()
     mission_profile_ind = MissionProfileField()
     table = forms.CharField(
         label="Input table",
@@ -94,43 +48,16 @@ class MultipleForm(forms.Form):
 class ParameterChoiceForm(forms.Form):
     def __init__(self):
         super().__init__()
-        self.models = []
-
-        try:
-            self.models = Model.objects.all()
-        except Exception as e:
-            logger.exception("Error retrieving models", exc_info=e)
+        self.models = Model.objects.all()
 
         for model in self.models:
-            # print(model.name, file=sys.stderr)
-            self.underlying_model = forms.TypedChoiceField(
-                label=r"Model",
-                choices=[(model.id, model.name) for model in self.models],
-                coerce=int,
-                empty_value=None
-            )
+            # Why is this defined within the loop?
+            self.underlying_model = ModelField(self.models)
 
             # self.precomputed_choices = [(i, r"$g_\star = %g$, $T_n = %g\, \mathrm{GeV}$" % (gstar,Tn)) for i, (gstar, Tn) in enumerate(zip(precomputed_gstar, precomputed_Tn))]
 
-            self.vw = forms.FloatField(
-                label=r"Wall velocity $v_\mathrm{w}$",
-                min_value=0.0,
-                max_value=1.0,
-                validators=[validate_velocity],
-                localize=False
-            )
-            # tstar = forms.FloatField(
-            #     label=r"Phase transition temperature $T_*$",
-            #     min_value=0.0
-            # )
-            self.alpha = forms.FloatField(
-                label=r"Phase transition strength $\alpha_\theta$",
-                min_value=0.0,
-                localize=False
-            )
-            self.beta_over_H = forms.FloatField(
-                label=r"Inverse phase transition duration $\beta/H_*$",
-                min_value=0.0,
-                localize=False
-            )
+            self.vw = VWField
+            # tstar = TStarField()
+            self.alpha = AlphaField()
+            self.beta_over_H = BetaOverHField()
             self.mission_profile = MissionProfileField()
