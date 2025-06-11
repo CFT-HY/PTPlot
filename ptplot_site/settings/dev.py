aeff201ca2f1397a6999ce478285146bc1d4973c
Mika Mäki <mika.x.maki@helsinki.fi>
2025-06-11 13:04:18 +0300

    Enable template debugging

--- ptplot_site/settings/dev.py
+++ ptplot_site/settings/dev.py
@@ -8,3 +8,5 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5)qw0w+z9)*q145l8$n_@d+$dj0f++p)npdmrddazr5j$yu869'
+
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
