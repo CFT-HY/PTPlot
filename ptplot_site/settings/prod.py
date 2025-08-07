6c62f65ee8f9e4c458c991bfdf46874cd6f957e8
Mika Mäki <mika.x.maki@helsinki.fi>
2025-08-07 14:26:26 +0200

    Allow embedding a local server in an iframe

--- ptplot_site/settings/prod.py
+++ ptplot_site/settings/prod.py
@@ -13,5 +13,9 @@ ALLOWED_HOSTS = [
 INTERNAL_IPS = ["127.0.0.1"]
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
+# Enabling X-Frame-Options only in production makes it possible
+# to embed a local development version of the site in an iframe for a reveal.js presentation.
+MIDDLEWARE.append("django.middleware.clickjacking.XFrameOptionsMiddleware")
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
