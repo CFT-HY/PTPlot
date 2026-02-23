c5be2b41e9bd0419e20cdf8b8737d03bc52f3045
Mika Mäki <mika.x.maki@helsinki.fi>
2026-02-23 15:05:05 +0200

    Improve testing of the science module

--- ptplot/tests/test_science.py
+++ ptplot/tests/test_science.py
@@ -10,56 +10,67 @@ from ptplot.science.spectrum.create import PowerSpectrumBPL, power_spectrum
 from ptplot.science.plot.snr_alpha_beta import snr_figure_alpha_beta
 from ptplot.science.plot.snr_ubarf_rstar import snr_figure_ubarf_rstar
 
-VW: float = 0.3
+V_WALL: float = 0.3
 ALPHA: float = 0.1
 BETA_OVER_H: float = 10000
+T_STAR: float = 100
+G_STAR: float = 100
 
 
 class ScienceTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.spectrum = PowerSpectrumBPL(vw=VW, alpha=ALPHA, beta_over_H=BETA_OVER_H)
+        cls.spectrum = PowerSpectrumBPL(vw=V_WALL, alpha=ALPHA, beta_over_H=BETA_OVER_H)
 
     def test_csv(self):
         self.spectrum.csv()
 
-    def test_kappa_v(self):
+    @staticmethod
+    def test_kappa_v():
         esp.kappa_v(v_wall=0.7, alpha_n=0.1)
 
-    def test_power_spectrum(self):
-        power_spectrum(vw=VW, alpha=ALPHA, beta_over_H=BETA_OVER_H)
+    @staticmethod
+    def test_power_spectrum():
+        power_spectrum(vw=V_WALL, alpha=ALPHA, beta_over_H=BETA_OVER_H)
 
-    def test_power_spectrum_dbpl(self):
-        power_spectrum(vw=VW, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.DBPL)
+    @staticmethod
+    def test_power_spectrum_dbpl():
+        power_spectrum(vw=V_WALL, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.DBPL)
 
-    def test_power_spectrum_ssm(self):
-        power_spectrum(vw=VW, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.SSM)
+    @staticmethod
+    def test_power_spectrum_ssm():
+        power_spectrum(vw=V_WALL, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.SSM)
 
-    def test_power_spectrum_ssm_const_cs(self):
-        power_spectrum(vw=VW, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.SSM, css2=1/4, csb2=1/4)
+    @staticmethod
+    def test_power_spectrum_ssm_const_cs():
+        power_spectrum(vw=V_WALL, alpha=ALPHA, beta_over_H=BETA_OVER_H, engine=Engine.SSM, css2=1/4, csb2=1/4)
 
     def test_ps_image(self):
         ps_figure(self.spectrum, sw_only=False)
 
-    def test_requirements(self):
+    @staticmethod
+    def test_requirements():
         req.main(print_points=False)
 
-    def test_snr_alpha_beta_image(self):
-        snr_figure_alpha_beta(v_wall_snr=0.3)
+    @staticmethod
+    def test_snr_alpha_beta_image():
+        snr_figure_alpha_beta(
+            v_wall_snr=V_WALL, T_star_snr=T_STAR, g_star_snr=G_STAR, alphas=ALPHA, beta_over_Hs=BETA_OVER_H
+        )
 
-    # def test_snr_curve(self):
-    #     snr_pre.get_SNRcurve()
+    @staticmethod
+    def test_snr_ubarf():
+        snr_figure_ubarf_rstar(
+            v_wall_snr=V_WALL, T_star_snr=T_STAR, g_star_snr=G_STAR,
+            alphas=ALPHA, beta_over_Hs=BETA_OVER_H, v_walls=V_WALL
+        )
 
-    def test_snr_ubarf(self):
-        snr_figure_ubarf_rstar(v_wall_snr=0.3)
-
-    # def test_stock_bkg_compute_snr(self):
-    #     snr.StockBkg_ComputeSNR()
-
-    def test_ubarf(self):
+    @staticmethod
+    def test_ubarf():
         esp.ubarf(v_wall=0.7, alpha_n=0.1)
 
-    def test_ubarf_to_alpha(self, vw: float = 0.7):
+    @staticmethod
+    def test_ubarf_to_alpha(vw: float = 0.7):
         ubarf = esp.ubarf(v_wall=0.7, alpha_n=0.1)
         esp.alpha_n_from_ubarf(ubarf, ubarf=ubarf)
